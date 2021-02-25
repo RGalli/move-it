aebaf37cf9965e35ad9d48331b55b178fa05406f
RGalli <ruan_hs_galli@hotmail.com>
2021-02-25 00:24:41 -0300

    Aula 03

--- src/components/CompleteChallenges.tsx
+++ src/components/CompleteChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompleteChallenges.module.css';
 
 export function CompleteChallenges() {
+  const { challengesCompleted } = useContext(ChallengeContext);
+
   return (
     <div className={styles.completeChallengesContainer}>
       <span>Desafios Completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompleteChallenges } from '../components/CompleteChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
@@ -18,7 +19,9 @@ export default function Home() {
           <CompleteChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
