7300fcccbc86f23e8912e9de21f26ac33dea0b7d
RGalli <ruan_hs_galli@hotmail.com>
2021-02-26 23:31:21 -0300

    Aula 03 e 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengeContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="http://github.com/rgalli.png" alt="Ruan Galli"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Ruan Galli</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
